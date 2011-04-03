a5429bdd489a0460229fc58610afc35e22bcd295
Philip (flip) Kromer <flip@infochimps.org>
2011-04-03 00:45:33 -0500

    added sample queries in menu bar

--- app/helpers/fight.rb
+++ app/helpers/fight.rb
@@ -3,9 +3,7 @@ class Main
   helpers do
     def link_to_fight topic_a, topic_b
       qq = build_query :topic_a => topic_a, :topic_b => topic_b
-      link_to "#{h topic_a} vs #{h topic_b}", "/?#{qq}"
+      link_to "#{h topic_a} v #{h topic_b}", "/?#{qq}"
     end
   end
 end
-
-p __FILE__
