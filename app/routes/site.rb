8adfd2350f816cbce3bb0fb5ef9f51141865e7b0
Philip (flip) Kromer <flip@infochimps.org>
2011-04-15 17:50:45 -0500

    oops that might be a bad idea with the caching

--- app/routes/site.rb
+++ app/routes/site.rb
@@ -1,9 +1,7 @@
 class ::Main < Sinatra::Base
-
   get "/" do
     @fight = Fight.from_hash(params)
     @dump = [ params, @fight.number_hits, @fight, ] # @fight.result_a
-    headers['Cache-Control'] = 'public, max-age=3600'
     haml :root
   end
 
